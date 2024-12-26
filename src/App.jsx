0c09028bc1886e478a12d637a55f1f4ed8c2969b
Maks <max.borovique@gmail.com>
2024-12-26 18:08:17 +0200

    add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <main className="relative w-screen min-h-screen overflow-x-hidden">
       <Hero />
+
+      <About />
     </main>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
-import gsap from 'gsap';
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -11,7 +14,7 @@ const Hero = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 3;
+  const totalVideos = 4;
   const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
   const nextVideoRef = useRef(null);
 
@@ -24,33 +27,68 @@ const Hero = () => {
     setCurrentIndex(upcomingVideoIndex);
   };
 
-  useGSAP(() => {
-    if(hasClicked){
-      gsap.set('#next-video', {visibility: 'visible'})
-      gsap.to('#next-video', {
-        transformOrigin: 'center center',
-        scale: 1,
-        width: '100%',
-        height: '100%',
-        duration: 1,
-        ease: 'power1.inOut',
-        onStart: () => nextVideoRef.current.play(),
-        
-      })
-
-      gsap.from(`#current-video`, {
-        transformOrigin: 'center center',
-        scale: 0, 
-        duration: 1.5,
-        ease: 'power1.inOut',
-        
-      })
+  useEffect(() => {
+    if(loadedVideos === totalVideos - 1) {
+      setIsLoading(false);
     }
-  },{dependencies: [currentIndex], revertOnUpdate: true})
+  }, [loadedVideos])
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+
+        gsap.from(`#current-video`, {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  );
+
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)",
+      borderRadius: "0 0 40% 10%",
+    });
+
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0 0 0 0",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
   return (
     <div className="relative h-dvh">
+      {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-x-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
       <div
         id="video-frame"
         className="relative z-10 w-screen overflow-hidden rounded-lg h-dvh bg-blue-75"
@@ -108,14 +146,19 @@ const Hero = () => {
               Enter the Metagame Layer <br /> Unleash the Play Economy
             </p>
 
-            <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1"/>
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="!bg-yellow-300 flex-center gap-1"
+            />
           </div>
         </div>
       </div>
 
-      <h1 className="absolutetext-black special-font hero-heading bottom-5 right-5">
-          G<b>a</b>ming
-        </h1>
+      <h1 className="absolute text-black special-font hero-heading bottom-5 right-5">
+        G<b>a</b>ming
+      </h1>
     </div>
   );
 };
